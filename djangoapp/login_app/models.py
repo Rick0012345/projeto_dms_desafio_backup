8399b55f3b223ce3a530950ad22176123109b689
patrick <po44243@gmail.com>
2024-08-14 09:53:16 -0300

    pequenas alterações

--- djangoapp/login_app/models.py
+++ djangoapp/login_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Campo(models.Model):
+    def registrar_campo(self):
+        pass
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    class Meta:
+        verbose_name = 'Campo'
+        verbose_name_plural = 'Campos'
+    def __str__(self):
+        return str(f"Latitude :{self.latitude} | Longitude: {self.longitude}")
