ab409f37734297d7e1b92af279539536a625ab3e
patrick <po44243@gmail.com>
2024-08-16 09:53:37 -0300

    cadastro de mapas funcionando

--- djangoapp/login_app/models.py
+++ djangoapp/login_app/models.py
@@ -8,5 +8,9 @@ class Coordenada(models.Model):
     class Meta:
         verbose_name = 'Coordenada'
         verbose_name_plural = 'Coordenadas'
+        
     def __str__(self):
         return str(f"Latitude :{self.latitude} | Longitude: {self.longitude}")
+    
+   
+   
--- djangoapp/login_app/views.py
+++ djangoapp/login_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import CoordenadaForm
+from .models import Coordenada
+import json
 # Create your views here.
 
 def loginPage(request):
@@ -17,12 +19,20 @@ def registerPage(request):
     return render(request,"account/signup.html")
 
 def areaProprietario(request):
+    query = Coordenada.objects.all()  
+    coordenadas = []
+    for i in query:
+        coordenadas.append({"latitude": i.latitude, "longitude": i.longitude})
+
+    print(query)
+
     if request.method == 'POST':
         form = CoordenadaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('coordenada_sucesso')
+            return redirect('alugar-campo')
     else:
         form = CoordenadaForm()
 
-    return render(request,"pages/alugarcamp.html", {'form': form})
+
+    return render(request,"pages/alugarcamp.html", {'form': form, 'coordenadas': json.dumps(coordenadas)})
